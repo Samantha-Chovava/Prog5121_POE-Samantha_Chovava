d463453a78f17dc4c8818ab477b118b8952165ec
Samantha-Chovava <st10505448@rcconnect.edu.za>
2026-09-16 12:17:29 +0200

    Created Chat Application Project

---
+++ src/main/java/com/mycompany/st10505448chatapplication/ST10505448ChatApplication.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.st10505448chatapplication;
+
+/**
+ *
+ * @author User
+ */
+public class ST10505448ChatApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
